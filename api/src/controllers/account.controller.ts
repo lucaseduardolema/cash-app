4dc5658dbffd07e5b51dab2c3590c97754e70acb
lucaseduardolema <lucaseduardolema@gmail.com>
2022-11-16 01:04:24 -0300

    feat: add GET balance token erros

--- api/src/controllers/account.controller.ts
+++ api/src/controllers/account.controller.ts
@@ -9,11 +9,12 @@ export default class AccountController {
 
   async getBalanceById(req: Request, res: Response) {
     const token = req.get("authorization") as string;
+    if (!token) throw new HttpError(404, "Token nessário")
 
     const user: IUser = decodeToken(token);
 
     const balance = await this.accountService.getBalanceById(user.accountId);
-    if (!balance) throw new HttpError(500, "Internal error");
+    if (!balance) throw new HttpError(401, "Token Inválido");
 
     res.status(200).json(balance);
   }
